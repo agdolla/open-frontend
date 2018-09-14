897d424cca1a7a93b355f923e02b0c16c84f5229
Trey Huffine <trey@postmates.com>
2018-09-14 10:25:45 -0700

    Types in utils

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { withRouter, RouteComponentProps } from 'react-static';
-import { Teams } from 'types';
+import { Teams } from 'utils/types';
 import { convertNameToSlug } from 'utils/helpers';
 
 import { List, Item, StyledLink } from './styles';
